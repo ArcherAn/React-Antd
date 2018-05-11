002905d119e1057b4dd686d764a08df6a7b2ffdf
ArcherAn <Archer_An@163.com>
2018-05-11 17:45:36 +0800

    布局更改

---
+++ src/containers/DashboardContainer1.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import '../App.css'
+import { Icon, Row, Col, Card, Steps, Button, message } from 'antd';
+
+
+/*
+
+   仪表盘1的 页面组件
+
+*/
+
+class DashboardContainer1 extends Component{
+
+    render(){
+        return(
+            <div>
+            </div>
+        )
+    }
+    
+}
+
+export default DashboardContainer1;
