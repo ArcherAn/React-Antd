4ef216df87a3e14e92c947553aba949ad5df7eba
ArcherAn <Archer_An@163.com>
2018-05-17 18:28:01 +0800

    提交

--- src/containers/Card/SalesCard.js
+++ src/containers/Card/SalesCard.js
@@ -23,28 +23,49 @@ class SalesCard extends Component{
             {year: '2015', 卫衣: 248, T恤: 217, 衬衫: 431}
         ];
     
-        const ds = new DataSet();
-        const dv = ds.createView().source(salescard);
-        dv.transform({
+        const data = [
+            { 'State': 'WY', '小于5岁': 25635, '5至13岁': 1890, '14至17岁': 9314 },
+            { 'State': 'DC', '小于5岁': 30352, '5至13岁': 20439, '14至17岁': 10225 },
+            { 'State': 'VT', '小于5岁': 38253, '5至13岁': 42538, '14至17岁': 15757 },
+            { 'State': 'ND', '小于5岁': 51896, '5至13岁': 67358, '14至17岁': 18794 },
+            { 'State': 'AK', '小于5岁': 72083, '5至13岁': 85640, '14至17岁': 22153 }
+          ];
+
+
+        const ds1 = new DataSet();
+        const dv1 = ds1.createView().source(salescard);
+        dv1.transform({
           type: 'fold',
           fields: [ '卫衣', 'T恤', '衬衫' ], // 展开字段集
           key: 'kinds', // key字段
           value: 'price', // value字段
         });
-        const cols = {
+        const cols1 = {
             year: {
             range: [ 0, 1 ]
           }
         }
 
+      const ds2= new DataSet();
+      const dv2 = ds2.createView().source(data);
+      dv2.transform({
+        type: 'fold',
+        fields: [ '小于5岁', '5至13岁', '14至17岁' ], // 展开字段集
+        key: '年龄段', // key字段
+        value: '人口数量', // value字段
+        retains: [ 'State' ] // 保留字段集，默认为除fields以外的所有字段
+      });
+
+
         return(
+          <div>
            <Row>
             <Col span={24}>
             <Card bordered={false} bodyStyle={{
               padding: '24px 36px 24px 0',
             }}>
                <div className='title' style={{paddingLeft:'20px'}}>销售统计</div>
-               <Chart height={270} data={dv} scale={cols} forceFit>
+               <Chart height={270} data={dv1} scale={cols1} forceFit>
                <Legend />
                <Axis name="year" />
                <Axis name="price" label={{formatter: val => `${val}`}}/>
@@ -55,6 +76,25 @@ class SalesCard extends Component{
                </Card>
             </Col>
           </Row>
+
+          <Row>
+            <Col span={24}>
+            <Card bordered={false} bodyStyle={{
+             padding: '24px 36px 24px 0',
+            }}>
+            <Chart height={260} data={dv2} forceFit>
+            <Legend />
+            <Coord transpose />
+            <Axis name="State" label={{offset: 12}} />
+            <Axis name="人口数量" />
+            <Tooltip />
+            <Geom type="intervalStack" position="State*人口数量" color={'年龄段'} />
+            </Chart>
+              
+           </Card>
+           </Col>
+          </Row>
+          </div>
         )
     }
     
